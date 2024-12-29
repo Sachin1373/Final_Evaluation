f573d641f8e74f82760d7f44f8da4f4c1217f596
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-12-30 01:42:18 +0530

    Form Response Schema added

---
+++ Backend/src/models/TypeBotResponses.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const responseSchema = new mongoose.Schema({
+  label: { type: String, required: true },
+  type: { type: String, required: true },
+  data: { type: mongoose.Schema.Types.Mixed, required: true }, 
+});
+
+const formResponseSchema = new mongoose.Schema(
+  {
+    formId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Form" },
+    viewCount: { type: Number, default: 0 },
+    startCount: { type: Number, default: 0 },
+    responses: [responseSchema],
+    submittedAt: { type: Date }, 
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("FormResponse", formResponseSchema);
