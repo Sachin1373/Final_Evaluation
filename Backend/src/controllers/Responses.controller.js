0b41ba7258b24494745efa8cc4fa239ecc20cd8c
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-12-31 13:46:25 +0530

    add response test 7

--- Backend/src/controllers/Responses.controller.js
+++ Backend/src/controllers/Responses.controller.js
@@ -27,19 +27,27 @@ import FormResponse from "../models/TypeBotResponses.model.js";
 
 
 
- export const addFormResponse = async (req, res) => {
+  export const addFormResponse = async (req, res) => {
     try {
       const { formId, responses } = req.body;
-      const responseEntry = {
-        date: new Date(), // current date
-        data: responses, // this is the array of responses passed from the client
-      };
+      
+      // Process the responses to ensure all data is in string format
+      const processedResponses = responses.map(item => ({
+        ...item,
+        data: String(item.data)
+      }));
   
+      const responseEntry = {
+        date: new Date(),
+        data: processedResponses,
+      };
+      
       await FormResponse.findOneAndUpdate(
         { formId },
         { $push: { responses: responseEntry } },
         { upsert: true }
       );
+      
       res.status(200).json({ message: 'Response added successfully' });
     } catch (error) {
       res.status(500).json({ error: error.message });
