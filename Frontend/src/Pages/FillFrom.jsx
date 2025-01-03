746b133917d8c868a53bed65385ffc92079a78aa
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2025-01-03 18:26:58 +0530

    response table correction

--- Frontend/src/Pages/FillFrom.jsx
+++ Frontend/src/Pages/FillFrom.jsx
@@ -431,7 +431,7 @@ function FillForm() {
         {renderInitialFields()}
         {formStage === 'main' && (
           <div className={styles.messagesContainer}>
-            {messages.slice(2).map((message, index) => (
+            {messages.map((message, index) => (
               <div key={index} className={styles.messageWrapper}>
                 {renderMessage(message, index)}
               </div>
